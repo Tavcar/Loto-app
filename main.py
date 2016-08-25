2a188d009904974a382cf71bc20fb48f319544cf
Marko Tavcar <mare185@gmail.com>
2016-08-25 19:03:34 +0200

    first

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import jinja2
+import webapp2
+import random
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if not params:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        return self.render_template("home.html")
+
+#x=random.sample(range(1, 39), 8)
+
+class LotoHandler(BaseHandler):
+    def get(self):
+        params = {"sporocilo": random.sample(range(1, 39), 8)}
+        return self.render_template("loto.html", params=params)
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+    webapp2.Route('/loto', LotoHandler),
+], debug=True)
